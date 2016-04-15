3ef76bc5e5eb29a409cbf04af8015f1005699ea6
dsherma7 <sherman.des@gmail.com>
2016-04-15 14:32:20 -0700

    Update VirtualMachine.cpp

--- Project2/VirtualMachine.cpp
+++ Project2/VirtualMachine.cpp
@@ -1,7 +1,14 @@
-extern c{     //Allow for backwards compabitlity with name mangling
+extern "c"{     //Allow for backwards compabitlity with name mangling
 
+//void MyCallback(void * cd, int result)
+//    Use this to handle different function call backs
+//(int?) MachineFileWrite(int fd, void * buffer, int len, void * callback, void * calldata)
 
-
+//Order to Start
+//Machine Initialize Starts the Machine 
+//VMStart()
+//Load Module (App) (ptr VMLoadModule(module.so))
+//Call ptr
 
 
 }
